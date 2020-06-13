bd1ab83d92dfaf6715d426fc68df1d6d4173cfe2
xamantra <xamantrx@gmail.com>
2020-06-13 15:02:17 +0800

    test: duplicate controller

--- test/momentum_test.dart
+++ test/momentum_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'utility.dart';
 import 'widgets/blank_widget.dart';
 import 'widgets/error_widget6.dart';
+import 'widgets/error_widget7.dart';
 
 void main() {
   testWidgets('null controller specified in momentum', (tester) async {
@@ -16,5 +17,11 @@ void main() {
     await inject(tester, widget);
     var blankText = find.text('Blank App');
     expect(blankText, findsOneWidget);
+  });
+
+  testWidgets('duplicate controller', (tester) async {
+    var widget = errorWidget7();
+    await inject(tester, widget);
+    expect(tester.takeException(), isInstanceOf<Exception>());
   });
 }
