2416d32fde028d653dd53974efb5bb8b2aa60e20
xamantra <xamantrx@gmail.com>
2020-06-13 14:58:06 +0800

    null controllers and services

--- test/momentum_test.dart
+++ test/momentum_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'utility.dart';
+import 'widgets/blank_widget.dart';
 import 'widgets/error_widget6.dart';
 
 void main() {
@@ -8,5 +9,12 @@ void main() {
     var widget = errorWidget6();
     await inject(tester, widget);
     expect(tester.takeException(), isInstanceOf<Exception>());
+  });
+
+  testWidgets('null controllers and services', (tester) async {
+    var widget = blankWidget();
+    await inject(tester, widget);
+    var blankText = find.text('Blank App');
+    expect(blankText, findsOneWidget);
   });
 }
