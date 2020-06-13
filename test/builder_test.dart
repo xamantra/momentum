f933a64b135cabc31c2022799123a60299354cff
xamantra <xamantrx@gmail.com>
2020-06-13 13:35:24 +0800

    non existent controller error test

--- test/builder_test.dart
+++ test/builder_test.dart
@@ -2,10 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'utility.dart';
 import 'widgets/error_widget.dart';
+import 'widgets/error_widget2.dart';
 
 void main() {
   testWidgets('null builder parameter', (tester) async {
     var widget = errorWidget();
+    await inject(tester, widget);
+    expect(tester.takeException(), isInstanceOf<Exception>());
+  });
+  testWidgets('non existent controller error test', (tester) async {
+    var widget = errorWidget2();
     await inject(tester, widget);
     expect(tester.takeException(), isInstanceOf<Exception>());
   });
