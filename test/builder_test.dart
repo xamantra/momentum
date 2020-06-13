6393b5ac4b2ddc2f73b8b423f058c2b288e533f8
xamantra <xamantrx@gmail.com>
2020-06-13 13:43:15 +0800

    dontRebuildIf test

--- test/builder_test.dart
+++ test/builder_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'components/counter/counter.controller.dart';
 import 'utility.dart';
 import 'widgets/error_widget.dart';
 import 'widgets/error_widget2.dart';
+import 'widgets/skip_rebuild.dart';
 
 void main() {
   testWidgets('null builder parameter', (tester) async {
@@ -14,5 +16,20 @@ void main() {
     var widget = errorWidget2();
     await inject(tester, widget);
     expect(tester.takeException(), isInstanceOf<Exception>());
+  });
+  testWidgets('dontRebuildIf test', (tester) async {
+    var widget = skipRebuildWidget();
+    await inject(tester, widget);
+    expect(find.text('0'), findsOneWidget);
+    var controller = widget.controllerForTest<CounterController>();
+    controller.increment();
+    await tester.pump();
+    expect(find.text('1'), findsOneWidget);
+    controller.increment();
+    await tester.pump();
+    expect(find.text('2'), findsOneWidget);
+    controller.increment();
+    await tester.pump();
+    expect(find.text('3'), findsNothing);
   });
 }
