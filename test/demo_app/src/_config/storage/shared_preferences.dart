647901ee5712c1641fc6ca575ec26ade10ee5641
xamantra <xamantrx@gmail.com>
2021-03-13 15:43:54 +0800

    removed unnecessary files inside test folder

--- test/demo_app/src/_config/storage/shared_preferences.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-SharedPreferences _sharedPreferences;
-SharedPreferences get sharedPreferences => _sharedPreferences;
-
-Future<void> initializeSharedPref() async {
-  _sharedPreferences = await SharedPreferences.getInstance();
-}
